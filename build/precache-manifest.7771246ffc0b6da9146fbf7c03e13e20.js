b824296d88d73b85dc8340e93d0330149286e0be
Flamur Bakiu <flamurbakiu.15@gmail.com>
2022-01-20 19:01:00 +0100

    Build for production

---
+++ build/precache-manifest.7771246ffc0b6da9146fbf7c03e13e20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b1f7e4b12224af39aaa5a79a48dc16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "149c159c779157d89427",
+    "url": "/static/js/2.5ffc14c3.chunk.js"
+  },
+  {
+    "revision": "d21b4bc0702397bf916425d3b382139b",
+    "url": "/static/js/2.5ffc14c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f5002e82cf706a0895",
+    "url": "/static/js/main.09024122.chunk.js"
+  },
+  {
+    "revision": "865fae79fab90ddfce54",
+    "url": "/static/js/runtime-main.422877f8.js"
+  }
+]);
